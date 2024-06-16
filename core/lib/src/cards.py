b704787eb4506b65705ba7e9ef5338d150d89cc0
Zotelkopf <nicolasflaemig@web.de>
2024-06-16 16:10:31 +0200

    Adds core.py and cards.py

---
+++ core/lib/src/cards.py
@@ -0,0 +1,24 @@
+class Card:
+    def __init__(self):
+        self.name = ""
+        self.cost = 0
+        
+    def __str__(self):
+        return self.name
+    
+    def recalculateCost(self):
+        pass 
+
+    def onGain(self):
+        pass
+
+    def onPlay(self):
+        pass
+
+    def onDiscard(self):
+        pass
+
+    def onTrash(self):
+        pass
+
+    
