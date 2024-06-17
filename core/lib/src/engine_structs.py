7f9f0886832e06b2ff2d7189dff700c0e01a06e2
Zotelkopf <nicolasflaemig@web.de>
2024-06-17 21:41:48 +0200

    Added logic for Supply and other things

--- core/lib/src/engine_structs.py
+++ core/lib/src/engine_structs.py
@@ -1,13 +1,23 @@
 from typing import Self, Callable
 from random import shuffle
 from card_models import *
-from core.lib.src.card_models import Card
+from cards import *
+
+
+class EmptySupplyExpection(Exception):
+    pass
+
+class WrongSupplyError(ValueError):
+    pass
+
+class EmptyDeckExpection(Exception):
+    pass
 
 
 class Cardbuffer:
     def __init__(self) -> None:
         self.cards: list[Card] = []
-        self.count = 0
+        self.count: int = 0
 
     def shuffle(self) -> None:
         shuffle(self.cards)
@@ -35,14 +45,6 @@ class Cardbuffer:
         else:
             raise ValueError
 
-    # needed for all Cardbuffers?
-    def drawTo(self, target: Self) -> None:
-        card: Card = self.removeTop()
-        if card is not None:
-            target.receive(card)
-        else:
-            raise ValueError
-
     def dumpTo(self, target: Self) -> None:
         card: Card = self.removeTop()
         while(card is not None):
@@ -65,27 +67,41 @@ class Cardbuffer:
             
 class SupplyPile(Cardbuffer):
     def __init__(self, card: Card, playerCount: int) -> None:
-        self.card = card
-        self.count = card.getSupplyCount(playerCount)
+        self.card: Card = card
+        self.count: int = card.getSupplyCount(playerCount)
         self.cards: list[Card] = [self.card] * self.count
 
     def receive(self, card: Card) -> None:
         if card == self.card:
             super().receive(card)
         else:
-            raise ValueError
+            raise WrongSupplyError
     
     def remove(self, card: Card) -> bool:
         if card == self.card:
             super().remove(card)
         else:
-            raise ValueError
+            raise WrongSupplyError
+        
+    def drawTo(self, target: Self) -> None:
+        card: Card = self.removeTop()
+        if card is not None:
+            target.receive(card)
+        else:
+            raise EmptySupplyExpection
         
     
 class Deck(Cardbuffer):
     def addToTop(self, card: Card) -> None:
         self.cards.insert(0, card)
         self.count += 1
+
+    def drawTo(self, target: Self) -> None:
+        card: Card = self.removeTop()
+        if card is not None:
+            target.receive(card)
+        else:
+            raise EmptyDeckExpection
         
 
 class InPlayBuffer(Cardbuffer):
@@ -93,8 +109,60 @@ class InPlayBuffer(Cardbuffer):
 
 
 class Supply:
-    def __init__(self) -> None:
+    def __init__(self, expansions: list[Set], playerCount: int) -> None:
+        self.supplies: dict[Card, SupplyPile] = {}
+        self.expansions: list[Set] = expansions
+        self.playerCount: int = playerCount
+
+        self.setup(expansions, playerCount)
+
+    def setup(self, expansions: list[Set], playerCount: int) -> None:
+        if Set.Alchemy in expansions: self.supplies[Potion()] = SupplyPile(Potion(), playerCount)
+        self.supplies[Copper()] = SupplyPile(Copper(), playerCount)
+        self.supplies[Silver()] = SupplyPile(Silver(), playerCount)
+        self.supplies[Gold()] = SupplyPile(Gold(), playerCount)
+        if Set.Prosperity in expansions: self.supplies[Platinum()] = SupplyPile(Platinum(), playerCount)
+
+        self.supplies[Curse()] = SupplyPile(Curse(), playerCount)
+        self.supplies[Estate()] = SupplyPile(Estate(), playerCount)
+        self.supplies[Duchy()] = SupplyPile(Duchy(), playerCount)
+        self.supplies[Province()] = SupplyPile(Province(), playerCount)
+        if Set.Prosperity in expansions: self.supplies[Colony()] = SupplyPile(Colony(), playerCount)
+
+        
+
+    def reset(self) -> None:
+        self.setup(self.expansions, self.playerCount)        
+
+    def getSupplyOf(self, card: Card) -> SupplyPile:
+        return self.supplies[card]
+    
+    def getCardsInSupply(self) -> list[Card]:
+        return self.supplies.keys
+    
+    async def gain(self, card: Card, location: Cardbuffer) -> None:
+        try:
+            self.getSupplyOf(card).drawTo(location)
+            await card.onGain()
+        except EmptySupplyExpection:
+            pass
+
+    async def buy(self, card: Card) -> None:
         pass
+    
+    
+
+class CardConditions:
+    def __init__(self) -> None:
+        self.expansions: list[Set] = []
+        self.minExpansions: int = 0
+        self.maxExpansions: int = 0
+        self.minCost: int = 0
+        self.maxCost: int = 0
+
+    def allowsFor(card: Card) -> bool:
+        pass
+
 
 
 class Player:
